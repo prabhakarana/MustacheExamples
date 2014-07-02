52ec8c5ac1caba64e938f995936c6abe441ba415
chatriyan <mailme@prabhakarana.com>
2014-07-02 11:05:02 -0400

    Comments - Initial Commit

---
+++ Helloworld-comments/js/custom.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Prabhakarana on 7/2/2014.
+ */
+function loadUser() {
+    var template = $('#template').html();
+    Mustache.parse(template);
+    var rendered = Mustache.render(template, "{}");
+    $('#target').html(rendered);
+}
+
+$(window).on('load', loadUser);
